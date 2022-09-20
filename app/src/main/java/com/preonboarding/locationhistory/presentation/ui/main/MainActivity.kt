f695bf02402d8a5c5c3e5945f704f1f80800ad3a
seoyoon513 <110798031+seoyoon513@users.noreply.github.com>
2022-09-20 14:21:19 +0900

    [feat] : webview 연결, GPS 연동

--- app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/preonboarding/locationhistory/presentation/ui/main/MainActivity.kt
@@ -1,14 +1,60 @@
 package com.preonboarding.locationhistory.presentation.ui.main
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.webkit.GeolocationPermissions
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.preonboarding.locationhistory.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webview_kakaomap)
+        webView.loadUrl("https://m.map.kakao.com/")
+
+        webView.webChromeClient = WebChromeClient() // 새로운 창을 띄우지 않고 내부에서 웹뷰 실행
+
+        // 웹뷰에서 GPS 사용
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onGeolocationPermissionsShowPrompt(
+                origin: String?,
+                callback: GeolocationPermissions.Callback?
+            ) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback)
+                callback?.invoke(origin, true, false)
+            }
+        }
+
+        webView.settings.javaScriptEnabled = true // 자바 스크립트 사용 허용
+
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                } else -> {
+                // No location access granted.
+            }
+            }
+        }
+
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
     }
 }
